2b4788f6fbb229a3d70789d3494b1ba9c885ba62
Ronaldo <ronaldoteran2407@gmail.com>
2025-10-13 17:12:11 -0500

    bloglist: create .env and move the MongoDB URI and PORT number to it. Create separate config.js module that handles importing dotenv and handling enviromental variables

--- bloglist/index.js
+++ bloglist/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 const blogsRouter = require("./controllers/blogs");
+const config = require("./utils/config");
 
 const app = express();
 
-const mongoUrl =
-	"mongodb+srv://ronaldoteran2407_db_user:Ron1998@cluster0.58lxoyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 (async () => {
 	try {
-		await mongoose.connect(mongoUrl);
+		await mongoose.connect(config.MONGODB_URI);
 	} catch (error) {
 		console.error("Error connecting to MongoDB:", error.message);
 		process.exit(1);
@@ -21,7 +20,6 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 
-const PORT = 3003;
-app.listen(PORT, ()=> {
-    console.log("Server running on port", PORT);
+app.listen(config.PORT, ()=> {
+    console.log("Server running on port", config.PORT);
 })
