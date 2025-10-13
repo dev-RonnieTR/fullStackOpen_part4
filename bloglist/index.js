2e5a5ad537f82f35ec435aa4f9eec734a198f253
Ronaldo <ronaldoteran2407@gmail.com>
2025-10-13 12:00:31 -0500

    initial commit

---
+++ bloglist/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl =
+	"mongodb+srv://ronaldoteran2407_db_user:Ron1998@cluster0.58lxoyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+(async () => {
+	try {
+		await mongoose.connect(mongoUrl);
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error.message);
+		process.exit(1);
+	}
+})();
+
+app.use(express.json());
+
+app.get("/api/blogs", async (req, res, next) => {
+	const blogs = await Blog.find({});
+	res.status(200).json(blogs);
+});
+
+app.post("/api/blogs", async (req, res, next) => {
+    const blog = new Blog(req.body);
+    const result = await blog.save();
+    res.status(201).json(result)
+})
+
+const PORT = 3003;
+app.listen(PORT, ()=> {
+    console.log("Server running on port", PORT);
+})
