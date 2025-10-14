150a858f4cbe55b5597bf27063a1a8c543056b8e
Ronaldo <ronaldoteran2407@gmail.com>
2025-10-14 11:10:34 -0500

    bloglist: move application definition to a separate module called app.js in the root directory

--- bloglist/app.js
+++ bloglist/app.js
@@ -1,8 +1,22 @@
-
+const express = require("express");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+const blogsRouter = require("./controllers/blogs");
 const config = require("./utils/config");
 
-const init = async () => {
-    try {
-        
-    }
-}
+const app = express();
+
+(async () => {
+	try {
+		await mongoose.connect(config.MONGODB_URI);
+	} catch (error) {
+		console.error("Error connecting to MongoDB:", error.message);
+		process.exit(1);
+	}
+})();
+
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- bloglist/index.js
+++ bloglist/index.js
@@ -1,25 +1,8 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-const blogsRouter = require("./controllers/blogs");
+const app = require("./app");
 const config = require("./utils/config");
 
-const app = express();
+const { PORT } = config;
 
-
-(async () => {
-	try {
-		await mongoose.connect(config.MONGODB_URI);
-	} catch (error) {
-		console.error("Error connecting to MongoDB:", error.message);
-		process.exit(1);
-	}
-})();
-
-app.use(express.json());
-
-app.use("/api/blogs", blogsRouter);
-
-app.listen(config.PORT, ()=> {
-    console.log("Server running on port", config.PORT);
-})
+app.listen(PORT, () => {
+	console.log("Connected to server on PORT", PORT);
+});
