84305c9a4691401fe2159d3b8fba8dd98ea2d852
Ronaldo <ronaldoteran2407@gmail.com>
2025-10-21 11:46:18 -0500

    bloglist: refactor controllers to have try/catch blocks that redirect to error handlers in cases of error

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -2,20 +2,32 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (req, res, next) => {
-	const blogs = await Blog.find({});
-	res.status(200).json(blogs);
+	try {
+		const blogs = await Blog.find({});
+		res.status(200).json(blogs);
+	} catch (error) {
+		next(error);
+	}
 });
 
 blogsRouter.get("/:id", async (req, res, next) => {
-	const blog = await Blog.findById(req.params.id);
-	res.status(200).json(blog);
+	try {
+		const blog = await Blog.findById(req.params.id);
+		res.status(200).json(blog);
+	} catch (error) {
+		next(error);
+	}
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-	req.body.likes = req.body.likes ?? 0;
-	const blog = new Blog(req.body);
-	const result = await blog.save();
-	res.status(201).json(result);
+	try {
+		req.body.likes = req.body.likes ?? 0;
+		const blog = new Blog(req.body);
+		const result = await blog.save();
+		res.status(201).json(result);
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = blogsRouter;
